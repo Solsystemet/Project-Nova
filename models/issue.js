d2df541480849083fb92ccead52398a3d3bb8160
MeisterRepsak <kasperschnejder@hotmail.com>
2024-03-27 13:51:36 +0100

    ah

--- models/issue.js
+++ models/issue.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const issueSchema = new mongoose.Schema({
-   title: String,
-   description: String,
-   createdData: String,
-   status: String,
+  title: String,
+  description: String,
+  createdData: String,
+  status: String,
 });
 
 module.exports = mongoose.model("Issue", issueSchema);
