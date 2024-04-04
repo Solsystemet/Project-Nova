34e6be9ba75a282f039569d919ce4a651c0b1dd6
lindejacob <lindejacob0612@gmail.com>
2024-04-04 11:51:39 +0200

    Fixed sidebar and made code better

--- public/js/todo.js
+++ public/js/todo.js
@@ -23,122 +23,48 @@ btnCreateIssue.addEventListener("click", (e) => {
   socket.emit("new task", value, description.textContent);
 });
 
-// Socket event listener for receiving a new task created by another user
+// Function to create a new task or task lane element
+
+// Socket event listener for new tasks
 socket.on("new task", (title, id, createDate) => {
-  // Create a new task element with provided data
-  const newTask = document.createElement("p");
-  newTask.id = id; // Set task ID
-  newTask.classList.add("card"); // Add CSS class for styling
-  newTask.setAttribute("draggable", true); // Allow task to be draggable
-
-  // Create task title element and set its content
-  const taskTitle = document.createElement("p");
-  taskTitle.classList.add("taskTitle");
-  taskTitle.textContent = title; // Task description/title
-  newTask.appendChild(taskTitle); // Append task title to task element
-
-  // Create task creation date element and set its content
-  const createdDate = document.createElement("p");
-  createdDate.classList.add("create-date");
-  createdDate.textContent = createDate; // Task creation date
-  newTask.appendChild(createdDate); // Append creation date to task element
-
-  //Assignee
-  const createdAssignee = document.createElement("img");
-  createdAssignee.classList.add("create-assignee");
-  //! Add connection to database & creation of the task
-  //? This is a test solution
-  createdAssignee.src = "../img/User/emptyPicture.png";
-  newTask.appendChild(createdAssignee);
-
-  // Issue footer
-  const issueFooter = document.createElement("div");
-  issueFooter.classList.add("issue-footer");
-  newTask.appendChild(issueFooter);
-
-  // Priority
-  const priority = document.createElement("div");
-  priority.classList.add("priority");
-  issueFooter.appendChild(priority);
-
-  //priority text
-  const priorityText = document.createElement("p");
-  priorityText.classList.add("priority-text");
-  //! Add connection to database & creation of the task
-  //? This is a test solution
-  priorityText.textContent = "High";
-  issueFooter.appendChild(priorityText);
-
-  // Due date
-  const dueDate = document.createElement("div");
-  dueDate.classList.add("due-date");
-  issueFooter.appendChild(dueDate);
-
-  //Due date text
-  const dueDateText = document.createElement("p");
-  dueDateText.classList.add("due-date-text");
-  //! Add connection to database & creation of the task
-  //? This is a test solution
-  dueDateText.textContent = "03/04/2024";
-  dueDate.appendChild(dueDateText);
-
-  //Tag
-  const createdTag = document.createElement("p");
-  createdTag.classList.add("create-tag");
-  //! Add connection to database & creation of the task
-  //? This is a test solution
-  createdTag.textContent = "Bug";
-  issueFooter.appendChild(createdTag);
-
-  // Add drag and drop event listeners to the new task element
-  newTask.addEventListener("dragstart", () => {
-    newTask.classList.add("is-dragging");
-  });
-
-  newTask.addEventListener("dragend", () => {
-    newTask.classList.remove("is-dragging");
-  });
-
-  // Append the new task element to the task lane/container
-  todoLane.appendChild(newTask);
-
-  // Update drag and drop functionality for all tasks
-  UpdateDragAndDrop();
-
-  // Close modal after task creation
+  const newTaskElement = createTaskElement(title, id, createDate);
+  todoLane.appendChild(newTaskElement); // Append the new task element to the task lane/container
+  UpdateDragAndDrop(); // Update drag and drop functionality for all tasks
   const modal = document.getElementById("modal");
-  closeModal(modal);
-
-  // Reset task description input field
-  description.textContent = "";
+  closeModal(modal); // Close modal after task creation
+  description.textContent = ""; // Reset task description input field
 });
 
 // Socket event listener for creating a new board (task lane)
 socket.on("create board", (title, id, createDate) => {
-  // Create a new task lane element with provided data (similar to creating a new task)
-  const newTask = document.createElement("p");
-  newTask.id = id;
-  newTask.classList.add("card");
-  newTask.setAttribute("draggable", true);
+  const newTaskLaneElement = createTaskElement(title, id, createDate);
+  todoLane.appendChild(newTaskLaneElement); // Append the new task lane element to the task lane/container
+  UpdateDragAndDrop(); // Update drag and drop functionality for all tasks
+});
+
+function createTaskElement(title, id, createDate) {
+  // Create a new task or task lane element with provided data
+  const newTask = document.createElement("div");
+  newTask.id = id; // Set task or task lane ID
+  newTask.classList.add("card"); // Add CSS class for styling
+  newTask.setAttribute("draggable", true); // Allow task or task lane to be draggable
 
   // Create task title element and set its content
   const taskTitle = document.createElement("p");
   taskTitle.classList.add("taskTitle");
   taskTitle.textContent = title; // Task description/title
-  newTask.appendChild(taskTitle); // Append task title to task element
+  newTask.appendChild(taskTitle); // Append task title to task or task lane element
 
   // Create task creation date element and set its content
   const createdDate = document.createElement("p");
-  createdDate.classList.add("create-date");
+  createdDate.classList.add("issue-date");
   createdDate.textContent = createDate; // Task creation date
-  newTask.appendChild(createdDate); // Append creation date to task element
+  newTask.appendChild(createdDate); // Append creation date to task or task lane element
 
-  //Assignee
+  // Assignee
   const createdAssignee = document.createElement("img");
-  createdAssignee.classList.add("create-assignee");
-  //! Add connection to database & creation of the task
-  //? This is a test solution
-  createdAssignee.src = "../img/User/emptyPicture.png";
+  createdAssignee.classList.add("issue-assignee");
+  createdAssignee.src = "../img/User/emptyPicture.png"; // Placeholder image
   newTask.appendChild(createdAssignee);
 
   // Issue footer
@@ -151,12 +77,10 @@ socket.on("create board", (title, id, createDate) => {
   priority.classList.add("priority");
   issueFooter.appendChild(priority);
 
-  //priority text
+  // Priority text
   const priorityText = document.createElement("p");
   priorityText.classList.add("priority-text");
-  //! Add connection to database & creation of the task
-  //? This is a test solution
-  priorityText.textContent = "High";
+  priorityText.textContent = "High"; // Default priority
   issueFooter.appendChild(priorityText);
 
   // Due date
@@ -164,23 +88,19 @@ socket.on("create board", (title, id, createDate) => {
   dueDate.classList.add("due-date");
   issueFooter.appendChild(dueDate);
 
-  //Due date text
+  // Due date text
   const dueDateText = document.createElement("p");
   dueDateText.classList.add("due-date-text");
-  //! Add connection to database & creation of the task
-  //? This is a test solution
-  dueDateText.textContent = "03/04/2024";
+  dueDateText.textContent = "03/04/2024"; // Default due date
   dueDate.appendChild(dueDateText);
 
-  //Tag
+  // Tag
   const createdTag = document.createElement("p");
-  createdTag.classList.add("create-tag");
-  //! Add connection to database & creation of the task
-  //? This is a test solution
-  createdTag.textContent = "Bug";
+  createdTag.classList.add("issue-tag");
+  createdTag.textContent = "Bug"; // Default tag
   issueFooter.appendChild(createdTag);
 
-  // Add drag and drop event listeners to the new task lane element
+  // Add drag and drop event listeners to the new task or task lane element
   newTask.addEventListener("dragstart", () => {
     newTask.classList.add("is-dragging");
   });
@@ -189,11 +109,6 @@ socket.on("create board", (title, id, createDate) => {
     newTask.classList.remove("is-dragging");
   });
 
-  // Append the new task lane element to the task lane/container
-  todoLane.appendChild(newTask);
-
-  // Update drag and drop functionality for all tasks
-  UpdateDragAndDrop();
-});
-
+  return newTask; // Return the created task or task lane element
+}
 //Thx Chatgpt for comment
